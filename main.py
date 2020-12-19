4b7e56051f035e4b205495c7716bbe5d345f14cb
KazukiOhta <kazukiota121@gmail.com>
2020-12-19 22:46:50 +0900

    Added: Procfile, debug = False.

--- main.py
+++ main.py
@@ -14,4 +14,4 @@ def delta():
     return jsonify(res)
 
 if __name__ == "__main__":
-    app.run(host = "0.0.0.0", port = 8080, debug = True)
+    app.run(host = "0.0.0.0", port = 8080, debug = False)
